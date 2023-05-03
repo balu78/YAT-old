4fd94bfbceaeb850cf26ad9d7fb43aef5b5e0de6
balu78 <chbrammohan@gmail.com>
2023-05-03 12:27:25 -0700

    Add files via upload
    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,54 @@
-# YAT-old
+# YeastAnalysisTool
+
+You need to make sure git, virtualenv, and python3 are installed and are in the $PATH (you can type those command names on the commandline and your computer finds them).
+
+
+
+For usage on a Mac, do the following in a terminal.
+
+
+git clone https://github.com/BrentLagesse/YeastAnalysisTool.git
+
+cd YeastAnalysisTool
+
+virtualenv venv
+
+source venv/bin/activate
+
+sh init.sh
+
+#NOTE:  If you are using an M1 mac, run sh initm1.sh instead.  
+
+You can do this while the other script is still running -- Download this link https://drive.google.com/file/d/1moUKvWFYQoWg0z63F0JcSd3WaEPa4UY7/view?usp=sharing and put it in the weights directory the previous script just made.
+
+python3 main.py 
+
+This starts the program.  Change the Input directory to one of your picture directories like M2210.  See Naming Scheme
+
+click start analysis and it should start going (the button will probably turn blue and stay that way a while. 
+
+After that runs (it'll take a few minutes or so, probably), you should see the images and the cells.  
+
+# Naming Scheme
+
+The system currently follows a strict naming scheme though we hope to make this more flexible in the future.
+
+In the main directory that you load in, you have a lot of files like this:
+
+2021_0902_M2351_    --- This is the BaseName of the files and can be anything
+This is immediately followed by a IdNumber like 001 (so if you had 8 images, you'd have 001 through 008)
+Then you have:  R3D_REF.tif
+
+So overall it looks like:   2021_0902_M2351_008_R3D_REF.tif
+
+Also in the main directory are subdirectories and they look like this -- 2021_0902_M2351_008_PRJ_TIFFS which is just the BaseName + IdNumber + _PRJ_TIFFS
+and that holds the additional images for each main image. 
+
+Those images should be named something like
+
+2021_0902_M2351_008_PRJ_w435.tif
+
+and there are 3 Type  --   w435 (DAPI), w525 (GFP), and w625 (mCherry) -- At one point we had support for CFP but it is not currently being used.
+
+which is just BaseName + IdNumber + _PRJ_ + Type + .tif
+
---
+++ mrcnn/preprocess_images.py
@@ -0,0 +1,77 @@
+import numpy as np
+from PIL import Image
+import os
+import skimage.exposure
+import skimage.filters
+from mrc import DVFile
+
+'''Convert input images to RGB format in separate folders required by MRCNN
+
+Arguments:
+inputdirectory: Input directory containing images.
+outputdirectory: Output directory to put new files in.
+outputfile: Path to save comma-delimited file that will tell the neural network the image paths.'''
+def preprocess_images(inputdirectory, mask_dir, outputdirectory, outputfile, verbose = False, use_cache=True):
+    if inputdirectory[-1] != "/":
+        inputdirectory = inputdirectory + "/"
+    if outputdirectory[-1] != "/":
+        outputdirectory = outputdirectory + "/"
+
+    if not os.path.exists(outputdirectory):
+        os.makedirs(outputdirectory)
+
+    output = open(outputfile, "w")
+    output.write("ImageId, EncodedRLE" + "\n")
+    output.close()
+
+    for imagename in os.listdir(inputdirectory):
+        if '_R3D_REF' not in imagename:
+            continue
+        extspl = os.path.splitext(imagename)
+        #check if there are .dv files and use them first
+        image = 0
+        if extspl[1] == '.dv':
+            f = DVFile(inputdirectory + imagename)   #the REF image should have only 1 picture in the stack
+            image = f.asarray()
+
+        #if we don't have .dv files, see if there are tifs in the directory with the proper name structure
+
+        elif len(extspl) != 2 or extspl[1] != '.tif':  # ignore files that aren't tifs
+            continue
+        else:
+            image = np.array(Image.open(inputdirectory + imagename))
+
+        try:
+            if verbose:
+                print ("Preprocessing ", imagename)
+            existing_files = os.listdir(mask_dir)
+            if imagename in existing_files and use_cache:   #skip this if we have a mask already
+                continue
+
+            if len(image.shape) > 2:
+                image = image[:, :, 0]
+            height = image.shape[0]
+            width = image.shape[1]
+
+            # Preprocessing operations
+            image = skimage.exposure.rescale_intensity(image.astype(np.float32), out_range=(0, 1))
+            image = np.round(image * 255).astype(np.uint8)        #convert to 8 bit
+            image = np.expand_dims(image, axis=-1)
+            rgbimage = np.tile(image, 3)                          #convert to RGB
+            #rgbimage = skimage.filters.gaussian(rgbimage, sigma=(1,1))   # blur it first?
+            imagename = imagename.split(".")[0]
+
+            if not os.path.exists(outputdirectory + imagename) or not use_cache:
+                os.makedirs(outputdirectory + imagename)
+                os.makedirs(outputdirectory + imagename + "/images/")
+            rgbimage = Image.fromarray(rgbimage)
+            rgbimage.save(outputdirectory + imagename + "/images/" + imagename + ".tif")
+
+            output = open(outputfile, "a")
+            output.write(imagename + ", " + str(height) + " " + str(width) + "\n")
+            output.close()
+        except IOError:
+            pass
+
+
+
